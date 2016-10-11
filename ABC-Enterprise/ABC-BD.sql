d2c894b3f2052d1a80a5c4ae4b07b89bdb19f4ee
joanmtg <joan62442@gmail.com>
2016-10-11 11:14:48 -0500

    Cambio en el SQL

--- ABC-Enterprise/ABC-BD.sql
+++ ABC-Enterprise/ABC-BD.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS Clientes, Gerentes, Vendedores, Jefes_taller, Sedes, Piezas, Automoviles, OrdenesTrabajo, Ventas, PiezasOrden  CASCADE;
+DROP TABLE IF EXISTS Clientes, Empleados, Sedes, Piezas, Automoviles, OrdenesTrabajo, Ventas, PiezasOrden  CASCADE;
 
 
 CREATE TABLE Sedes (
@@ -18,8 +18,8 @@ CREATE TABLE Clientes (
 	CONSTRAINT pk_cliente PRIMARY KEY(id_cliente)
 );
 
-CREATE TABLE Gerentes (
-	cod_gerente char(5),
+CREATE TABLE Empleados (
+	cod_empleado char(5),
 	nombre varchar(80) NOT NULL,
 	password varchar(20) NOT NULL,
 	edad char(2) NOT NULL,
@@ -28,42 +28,11 @@ CREATE TABLE Gerentes (
 	email varchar(30) NOT NULL,
 	titulo varchar(30) NOT NULL,
 	direccion varchar(50) NOT NULL,
+	tipo varchar(20) NOT NULL,
 	cod_sede char(5),
 	CONSTRAINT fk_sedeG FOREIGN KEY(cod_sede)
 	REFERENCES Sedes(cod_sede),
-	CONSTRAINT pk_gerente PRIMARY KEY(cod_gerente)
-);
-
-CREATE TABLE Vendedores (
-	cod_vendedor char(5),
-	nombre varchar(80) NOT NULL,
-	password varchar(20) NOT NULL,
-	edad char(2) NOT NULL,
-	estado char(1) NOT NULL,
-	telefono varchar(10) NOT NULL,
-	email varchar(30) NOT NULL,
-	titulo varchar(30) NOT NULL,
-	direccion varchar(50) NOT NULL,
-	cod_sede char(5),
-	CONSTRAINT fk_sedeV FOREIGN KEY(cod_sede)
-	REFERENCES Sedes(cod_sede),
-	CONSTRAINT pk_vendedor PRIMARY KEY(cod_vendedor)
-);
-
-CREATE TABLE Jefes_taller (
-	cod_jefeT char(5),
-    nombre varchar(80) NOT NULL,
-    password varchar(20) NOT NULL,
-    edad char(2) NOT NULL,
-    estado char(1) NOT NULL,
-    telefono char(10) NOT NULL,
-    email varchar(30) NOT NULL,
-    titulo varchar(30) NOT NULL,
-    direccion varchar(50) NOT NULL,
-    cod_sede char(5), 
-    CONSTRAINT fk_sedeJt FOREIGN KEY(cod_sede)
-    REFERENCES Sedes(cod_sede),
-    CONSTRAINT pk_jefeT PRIMARY KEY(cod_jefeT)
+	CONSTRAINT pk_empleado PRIMARY KEY(cod_empleado)
 );
 
 
@@ -89,26 +58,26 @@ CREATE TABLE OrdenesTrabajo (
 	cod_orden char(5),
 	estado char(1) NOT NULL,
 	descripcion varchar(100),
-	cod_jefeT char(5),
-	CONSTRAINT fk_jefeOrden FOREIGN KEY(cod_jefeT)
-	REFERENCES Jefes_taller(cod_jefeT),
+	cod_empleado char(5),
+	CONSTRAINT fk_empleado FOREIGN KEY(cod_empleado)
+	REFERENCES Empleados(cod_empleado),
 	CONSTRAINT pk_orden PRIMARY KEY(cod_orden)
 );
 
 CREATE TABLE Ventas (
 	cod_auto char(5),
-	cod_vendedor char(5),
+	cod_empleado char(5),
 	id_cliente char(10),
 	fecha varchar(10) NOT NULL,
 	total int NOT NULL,
 	CONSTRAINT fk_ventaA FOREIGN KEY(cod_auto)
 	REFERENCES Automoviles(cod_auto),
-	CONSTRAINT fk_ventaV FOREIGN KEY(cod_vendedor)
-	REFERENCES Vendedores(cod_vendedor),
+	CONSTRAINT fk_ventaV FOREIGN KEY(cod_empleado)
+	REFERENCES Empleados(cod_empleado),
 	CONSTRAINT fk_ventaC FOREIGN KEY(id_cliente)
 	REFERENCES Clientes(id_cliente),
 	CONSTRAINT pk_venta 
-	PRIMARY KEY(cod_auto, cod_vendedor, id_cliente)
+	PRIMARY KEY(cod_auto, cod_empleado, id_cliente)
 );
 
 CREATE TABLE PiezasOrden (
@@ -121,4 +90,6 @@ CREATE TABLE PiezasOrden (
 	CONSTRAINT pk_piezasO PRIMARY KEY(cod_pieza, cod_orden)
 );
 
-INSERT INTO sedes(cod_sede, nombre, direccion) VALUES ('00001', 'Sede del infierno', 'Cuarto círculo');
+INSERT INTO sedes(cod_sede, nombre, direccion) VALUES (1, 'Sede de Cali', 'Univalle');
+
+INSERT INTO Gerentes(cod_gerente, nombre, password, edad, estado, telefono, email, titulo, direccion, cod_sede) VALUES ('00001', 'Linus Torvalds', '00001-30', 30, 1, 3335678, 'linus@likeaboss.com', 'Doctor', 'Ciudad Jardin', '00001');
